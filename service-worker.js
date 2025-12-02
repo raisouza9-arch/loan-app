5fb8dd5f2b23262e54fbf413eac747933cd71436
raisouza9-arch <raisouza9@icloud.com>
2025-12-02 14:43:24 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('loan-app-cache').then(cache => {
+      return cache.addAll(['./', './index.html', './manifest.json']);
+    })
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
